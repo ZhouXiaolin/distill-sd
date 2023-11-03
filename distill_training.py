a59bd8ce685eddfdec6dab1d7cc08ea64584a968
zhouxiaolin <zhouxiaolinux@gmail.com>
2023-11-04 03:40:40 +0800

    add dalle2

--- distill_training.py
+++ distill_training.py
@@ -788,7 +788,7 @@ def main():
         # Downloading and loading a dataset from the hub.
         dataset = load_dataset(
             args.dataset_name,
-            data_dir=['data/train-00000-of-00024.parquet'],
+            data_files=['data/train-00000-of-00024.parquet'],
             cache_dir=args.cache_dir,
         )
     else:
